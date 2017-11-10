973f4a671711f4ac5f25e032c2ec797dc06a3908
Kelly Moore <kellym@waffles.local>
2017-11-10 14:11:56 +0000

    More questions added: sick, timesheet deadline, pmo info ..

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const intents = new builder.IntentDialog({
         new builder.LuisRecognizer(process.env.LUIS_ENDPOINT)
             ],
     intentThreshold: 0.4,
-         recognizeOrder: builder.RecognizeOrder.series
+    recognizeOrder: builder.RecognizeOrder.parallel
 });
 
 intents.matches('greeting', '/greeting');
@@ -36,6 +36,13 @@ intents.matches('buySellHoliday', '/buySellHoliday');
 intents.matches('mandatoryField', '/mandatoryField');
 intents.matches('dayInLieu', '/dayInLieu');
 intents.matches('missingTimecode', '/missingTimecode');
+intents.matches('sick', '/sick');
+intents.matches('timesheetDeadline', '/timesheetDeadline');
+intents.matches('carryHolidays', '/carryHolidays');
+intents.matches('bookHolidays', '/bookHolidays');
+intents.matches('claimExpenses', '/claimExpenses');
+intents.matches('pmoInfo', '/pmoInfo');
+
 intents.matches('support', require('./support'));
 
 
@@ -55,42 +62,34 @@ bot.dialog('/greeting', function(session){
 
 bot.dialog('/prevYearHol', function(session){
     session.endDialog("All of your remaining holiday totals accumulate to one total which can be accessed by using the current holiday code");
-
 });
 
 bot.dialog('/emptyDashboard', function(session){
     session.endDialog("We have missed some settings on your account, message PMO@kainos.com to fix this.");
-
 });
 
 bot.dialog('/holidayCode', function(session){
     session.endDialog("Please make sure you are submitting your holidays through Gridview.  Everyone has a holiday code.");
-
 });
 
 bot.dialog('/cancelHoliday', function(session){
     session.endDialog("Your timesheet will need to be reverted please contact: the following: \n - Digital Services: digitalservicespmo@kainos.com \n - Smart: j.carson@kainos.com \n - Everyone else: PMO@Kainos.com ");
-
 });
 
 bot.dialog('/approvePrivileges', function(session){
     session.endDialog("Your permissions will need to be upgraded, depending on your grade this may need approval from your manager.");
-
 });
 
 bot.dialog('/buySellHoliday', function(session){
     session.endDialog("Request approval from your manager for the amount of days you wish to buy/sell, forward the approval email to People Support.");
-
 });
 
 bot.dialog('/mandatoryField', function(session){
     session.endDialog("You have missed a mandatory field on your entry, please check these are all complete - the best way to view this is calendar view.");
-
 });
 
 bot.dialog('/dayInLieu', function(session){
     session.endDialog(" Request approval for the day in lieu from your manager, forward the approval email to People Support");
-
 });
 
 bot.dialog('/missingTimecode', function(session){
@@ -100,6 +99,32 @@ bot.dialog('/missingTimecode', function(session){
 bot.dialog('/contacts', function(session){
     session.endDialog('Please contact: the following: \n - Digital Services: digitalservicespmo@kainos.com \n - Smart: Jayne Carson j.carson@kainos.com \n - Everyone else - PMO@Kainos.com');
 });
+
+bot.dialog('/sick', function(session){
+    session.endDialog("Please use your BU's Sick code (e.g. Digital Services - Sick) If you will be off sick for more than the standard 5 days (self certifiable) you will need to contact People Support with a Doctors note. ");
+});
+
+bot.dialog('/timesheetDeadline',function(session){
+    session.endDialog("Please fill in and submit your timesheet by CoB on a Friday, if the month ends midweek, please ensure your timesheets are completed for the whole month and submit before CoB on the last day of the month. ");    
+});
+
+bot.dialog('/carryHolidays',function(session){
+    session.endDialog("The maximum number of holiday days that can be carried over to the next year is 5. Please contact your PM if you need to carry over more than 5 days.");    
+});
+
+bot.dialog('/bookHolidays',function(session){
+    session.endDialog("Please book all holidays through your Kimble timesheet in Grid View mode. Any holidays submitted via Calendar view will not register correctly. Add holidays as you would a standard work day and click submit, these will then be sent through to your Kimble manager for approval. ");    
+});
+
+bot.dialog('/claimExpenses',function(session){
+    session.endDialog("Expenses are claimed through Kimble against the project code they were incurred against. For expenses guildlines please see the guildline documents for specific locations on the Finance OneDrive page - https://tinyurl.com/ya98rrtp ");    
+});
+
+bot.dialog('/pmoInfo',function(session){
+    session.endDialog('PMO is the "Project Management Office" within Kainos. For more information and an expample of PMO updates, please check out the PMO page on Yammer. ');    
+});
+
+
 
 bot.dialog('/holiday', [function(session){
     builder.Prompts.choice(session,"I know the following about holidays. Please select an option below to find out more informaion:", "Buy/Sell Holidays|Cancel Holidays|Holiday Timecode|Carrying Over Holidays | Days in Lieu | I need more information", { listStyle: builder.ListStyle.button }); 
@@ -126,23 +151,5 @@ function(session, results){
             break;
     }
 }]).triggerAction({
-    matches: [/holiday/i, /holidays/i, /annual leave/i]
+    matches: [/^holiday/i, /^holidays/i, /^annual leave/i]
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
